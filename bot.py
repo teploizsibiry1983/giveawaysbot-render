f2406e1712b631def45595f0329d84451ceacc14
teploizsibiry1983 <teploizsibiry@yandex.ru>
2025-12-09 14:39:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from config import BOT_TOKEN
 # создаём приложение
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# сюда добавляем свои обработчики
+# сюда добавляем обработчики
 # app.add_handler(CommandHandler("start_drawing", start_drawing_function))
 # app.add_handler(CallbackQueryHandler(button_click_function))
 
